e76dc745145f225f6367d902f2bcfe86e76e00ee
Axel Holzinger <aholzinger@gmx.de>
2020-03-21 20:57:26 +0100

    json-validator.cpp(594): error C2593: 'operator =' is ambiguous.

--- Development/third_party/nlohmann/json-validator.cpp
+++ Development/third_party/nlohmann/json-validator.cpp
@@ -591,8 +591,8 @@ public:
 #ifndef NO_STD_REGEX
 		attr = sch.find("pattern");
 		if (attr != sch.end()) {
-			patternString_ = attr.value();
-			pattern_ = {true, REGEX_NAMESPACE::regex(attr.value().get<std::string>(),
+			patternString_ = attr.value().get<std::string>();
+			pattern_ = {true, REGEX_NAMESPACE::regex(patternString_,
 			                                         REGEX_NAMESPACE::regex::ECMAScript)};
 			sch.erase(attr);
 		}
