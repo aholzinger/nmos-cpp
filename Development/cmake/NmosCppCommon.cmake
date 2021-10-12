e3c3ba9ae209093857e324e53de9373bc04b4535
Axel Holzinger <aholzinger@gmx.de>
2021-10-12 15:58:46 +0200

    Use NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME (when not using vcpkg) to link statically to the C/C++ runtime under Windows. Use NMOS_CPP_USE_VCPKG (and set NMOS_CPP_USE_CONAN to False) to use vcpkg instead of conan. When using vcpkg please also specify VCPKG_TARGET_TRIPLET (using x64-windows-static or x86-windows-static then chooses static linkage to the C/C++ runtime). Don't use NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME when using vcpkg.

--- Development/cmake/NmosCppCommon.cmake
+++ Development/cmake/NmosCppCommon.cmake
@@ -1,8 +1,47 @@
+# otherwise boost will be found in the local file system instead of conan or vcpkg
+set(ENV{BOOST_ROOT})
+
+set(NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME OFF CACHE BOOL "Link statically to C/C++ runtime on Windows")
+mark_as_advanced(FORCE NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME)
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    if(NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME AND DEFINED VCPKG_TARGET_TRIPLET)
+        message(FATAL_ERROR "NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME and VCPKG_TARGET_TRIPLET both set! Either use Conan with NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME or use Vcpkg without NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME.")
+    endif()
+    if(NMOS_CPP_USE_WINDOWS_STATIC_RUNTIME)
+        message(STATUS "Linking statically to C/C++ runtime on Windows")
+        # switch option CMP0091
+        # if (DEFINED WIN32) # unfortunately CMAKE_SYSTEM_NAME is not yet set here
+            if(POLICY CMP0091)
+                cmake_policy(SET CMP0091 NEW)
+                set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+            else()
+                message(FATAL_ERROR "Policy CMP0091 not vailable. You need CMake 3.16 or newer to configure static runtime linkage on Windows!")
+            endif()
+        # endif()
+    endif()
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /DBOOST_SYSTEM_USE_UTF8")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /DBOOST_SYSTEM_USE_UTF8")
+endif()
+
 set(NMOS_CPP_USE_CONAN ON CACHE BOOL "Use Conan to acquire dependencies")
 mark_as_advanced(FORCE NMOS_CPP_USE_CONAN)
 
+set(NMOS_CPP_USE_VCPKG OFF CACHE BOOL "Use Vcpkg to acquire dependencies")
+mark_as_advanced(FORCE NMOS_CPP_USE_VCPKG)
+
+if(NMOS_CPP_USE_CONAN AND NMOS_CPP_USE_VCPKG)
+    message(FATAL_ERROR "Either choose NMOS_CPP_USE_CONAN for Conan or NMOS_CPP_USE_VCPKG for Vcpkg to acquire libraries")
+endif()
+
 if(NMOS_CPP_USE_CONAN)
+    message(STATUS "Using Conan to acquire dependencies")
     include(cmake/NmosCppConan.cmake)
+endif()
+
+if(NMOS_CPP_USE_VCPKG)
+    message(STATUS "Using Vcpkg to acquire dependencies")
+    include(cmake/NmosCppVcpkg.cmake)
 endif()
 
 include(GNUInstallDirs)
